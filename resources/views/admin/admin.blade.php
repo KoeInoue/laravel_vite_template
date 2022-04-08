ebfdc8ca57bdd701e290c7fee2734cb44a292377
Ko Inoue <koinoue@Kos-MacBook-Pro.local>
2022-04-08 19:03:44 +0700

    Add admin sample

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -1,14 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>ダッシュボード</h1>
+@stop
+
+@section('content')
     @foreach ($admins as $admin)
-        <p>{{ $admin }}</p>
-    @endforeach
-</body>
-</html>
+    <p>{{ $admin }}</p>
+@endforeach
+@stop
+
+@section('css')
+    {{-- css per page
+    <link rel="stylesheet" href="/css/xxx.css">
+    --}}
+@stop
+
+@section('js')
+    <script> console.log('JS per page'); </script>
+@stop
